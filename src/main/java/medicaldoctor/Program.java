2e3bf7608efebb6db2d44aad92f191c303289ad8
Thomas Lacasse <thomas@kukt.us>
2020-04-06 15:51:30 -0400

    Revert "Made Controller Modifcations"
    This reverts commit 4fc6201414c02df1d1fc5ace217310a2cb5423ed.

--- src/main/java/medicaldoctor/Program.java
+++ src/main/java/medicaldoctor/Program.java
@@ -1,37 +1,18 @@
 package medicaldoctor;
 
-import com.sun.glass.ui.Screen;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import medicaldoctor.core.DatabaseScope;
 
 public class Program extends Application {
-    
-    //Use better methold than this
-    public static String ScreenLoginName = "Login";
-    private static String ScreenLogin = "Login.fxml";
-    public static String ScreenSecondLoginName = "S_Login";
-    private static String ScreenSecondLogin = "S_Login.fxml";
-    public static String ScreenMainScreenName = "MainScreen";
-    private static String ScreenMainScreen = "MainScreen.fxml";
 
     @Override
-    public void start(Stage stage){
-        ControllerManager cm = new ControllerManager();
-        cm.load(Program.ScreenLoginName, Program.ScreenLogin);
-        cm.load(ScreenSecondLoginName, ScreenSecondLogin);
-        cm.load(Program.ScreenMainScreenName, Program.ScreenMainScreen);   
-        cm.showScreen(Program.ScreenLoginName);
-        Group root = new Group();
-        root.getChildren().addAll(cm);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
         Scene scene = new Scene(root);
-        stage.setFullScreen(true);
-        stage.setResizable(false);
-        
         stage.setScene(scene);
         stage.show();
     }
@@ -46,5 +27,5 @@ public class Program extends Application {
             DatabaseScope._shutdown();
         }
     }
-    
+
 }
