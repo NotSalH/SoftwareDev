755cfe4d6f43590b3726b344e0000430484710bb
Thomas Lacasse <thomas@kukt.us>
2020-04-06 22:30:32 -0400

    feat(navbar): example navbar include

---
+++ src/main/java/medicaldoctor/NavigationBarController.java
@@ -0,0 +1,74 @@
+package medicaldoctor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import medicaldoctor.core.AppSession;
+import medicaldoctor.core.Permission;
+
+public class NavigationBarController {
+
+    @FXML
+    private Button buttonMRI;
+
+    @FXML
+    private Button buttonXRay;
+
+    @FXML
+    private Button buttonWhiteBloodCell;
+
+    @FXML
+    private Button buttonMyLabRequests;
+
+    @FXML
+    private Button buttonUrinaryTest;
+
+    @FXML
+    private Button buttonDoctorLookup;
+
+    @FXML
+    private Button buttonLabRequests;
+
+    @FXML
+    private Button buttonStoolTest;
+
+    @FXML
+    private Button buttonPatientLookup;
+
+    @FXML
+    private Button buttonUserLookup;
+
+    @FXML
+    private Button buttonPatientRegistration;
+
+    @FXML
+    private Button buttonRegisterNewUser;
+
+    @FXML
+    private Button buttonCT;
+
+    @FXML
+    private Button buttonMyPatients;
+
+    @FXML
+    private Button buttonRedBloodCell;
+
+    @FXML
+    private Button buttonElectrolyteTest;
+
+    @FXML
+    private Button buttonLabPatients;
+
+    @FXML
+    private Button buttonLogs;
+
+    @FXML
+    private Button buttonRenalFunction;
+
+    @FXML
+    private Button buttonLiverFunction;
+
+    @FXML
+    public void initialize() {
+    }
+
+}
---
+++ src/main/java/medicaldoctor/TempNavBarExampleProgram.java
@@ -0,0 +1,35 @@
+package medicaldoctor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import medicaldoctor.core.DatabaseScope;
+
+/**
+ * Temporary runnable to show how javafx includes work for the navbar, as we do
+ * not have the scene transitions after login yet.
+ */
+public class TempNavBarExampleProgram extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("AdminDashboard.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        try {
+            launch(args);
+        } finally {
+            DatabaseScope._shutdown();
+        }
+    }
+
+}
