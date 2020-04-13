43acc9ee097a8f0e654c10851cc79e9006e2f52d
Thomas Lacasse <thomas@kukt.us>
2020-04-12 22:34:34 -0400

    feat(profile): complete

--- src/main/java/medicaldoctor/controllers/ProfileController.java
+++ src/main/java/medicaldoctor/controllers/ProfileController.java
@@ -12,45 +12,37 @@ import medicaldoctor.core.AppSession;
 import medicaldoctor.core.Permission;
 
 public class ProfileController implements Initializable {
-
+    
     @FXML
-    TextField username_tf, first_name_tf, last_name_tf, employee_id_t, email_tf, phone_tf, office_tf, employee_type_tf, sex_tf;
-
+    TextField username_tf, first_name_tf, last_name_tf, employee_id_t, email_tf, department_tf, office_tf, employee_type_tf;
+    
     @FXML
     Label no_field;
-
+    
     @FXML
     Button conformation, new_user_id, employee_id_tf;
-
+    
     @FXML
     void new_user(ActionEvent event) throws Exception {
         AppSession.CONTROLLER_MANAGER.load(LookUp.REGISTER_NEW_USER);
         AppSession.CONTROLLER_MANAGER.showScreen(LookUp.REGISTER_NEW_USER);
     }
-
+    
     @FXML
     void change_password(ActionEvent event) {
-
+        
     }
-
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        buttonMustHaveThisPermission(conformation, Permission.REGISTER_NEW_USER);
-        buttonMustHaveThisPermission(new_user_id, Permission.REGISTER_NEW_USER);
-        buttonMustHaveThisPermission(employee_id_tf, Permission.REGISTER_NEW_USER);
         first_name_tf.setText(AppSession.getActiveUser().getFirstName());
         last_name_tf.setText(AppSession.getActiveUser().getLastName());
         username_tf.setText(AppSession.getActiveUser().getUserName());
         employee_id_t.setText(AppSession.getActiveUser().getId() + "");
-        phone_tf.setText(AppSession.getActiveUser().getOfficeNum() + "");
-        office_tf.setText(AppSession.getActiveUser().getDepartment());
+        email_tf.setText(AppSession.getActiveUser().getEmail());
+        department_tf.setText(AppSession.getActiveUser().getDepartment());
+        office_tf.setText(AppSession.getActiveUser().getOfficeNum() + "");
         employee_type_tf.setText(AppSession.getActiveUser().getType().getName());
     }
-
-    private void buttonMustHaveThisPermission(Button button, Permission permission) {
-        if (AppSession.getActiveUser().hasPermission(permission) == false) {
-            button.setManaged(false);
-        }
-    }
-
+    
 }
